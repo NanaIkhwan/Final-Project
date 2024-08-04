f1bfdeec519089c488c1644378f398078459a067
Najwa <nanaikhwanm69@gmail.com>
2024-08-04 23:35:25 +0700

    first commit

---
+++ Soal234/soal-3/soal-3.ts
@@ -0,0 +1,11 @@
+function printTriangle(height: number): void {
+    for (let i = 1; i <= height; i++) {
+        let row = '';
+        for (let j = 1; j <= i; j++) {
+            row += '*';
+        }
+        console.log(row);
+    }
+}
+
+printTriangle(10);
